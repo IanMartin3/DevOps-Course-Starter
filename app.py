488b699dadc25863f3b4bdc166e19e7caa26a7a9
Ian Martin <ian.martin3@capita.com>
2020-09-22 10:09:19 +0100

    Removed Button Group and switched to individual buttons

--- app.py
+++ app.py
@@ -4,11 +4,15 @@ import session_items as session
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
 
-@app.route('/', methods = ['POST', 'GET'])
+@app.route('/', methods = ['POST', 'GET', 'PUT', 'DELETE'])
 def index():
     if request.method == 'POST':
         NewItem = request.form["NewItem"]
         session.add_item(NewItem)
+        return render_template('index.html', items = session.get_items())
+    elif request.method == 'PUT':
+        SelectedItem = requestform["CompleteTask"]
+        session.save_item()
         return render_template('index.html', items = session.get_items())
     else:
         return render_template('index.html', items = session.get_items())
