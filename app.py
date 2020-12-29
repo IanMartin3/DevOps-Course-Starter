8521db6398bfdd469abf0507e1ade241d1ddd19c
Ian Martin <ian.martin3@capita.com>
2020-12-29 15:52:05 +0000

    removed trello api as now seperate file

--- app.py
+++ app.py
@@ -12,66 +12,7 @@ _DEFAULT_ITEMS = [
 
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
-"""
-get_boards = "https://api.trello.com/1/members/me/boards"
 
-
-headers = {
-    "Accept": "application/json"
-}
-
-query = {
-    'key': key,
-    'token': token
-}
-
-all_boards = requests.request(
-   "GET",
-   get_boards,
-   headers=headers,
-   params=query
-)
-
-board_id = all_boards.json()[0]['id']
-print(board_id)
-generate_list_string = "https://api.trello.com/1/boards/{}/lists/".format(board_id)
-
-
-all_lists = requests.request(
-   "GET",
-   generate_list_string,
-   headers=headers,
-   params=query
-)
-
-lists = all_lists.json()
-
-for list in lists:
-    print(list)
-
-#print(json.dumps(json.loads(all_lists.text), sort_keys=True, indent=4, separators=(",", ": ")))
-
-#all_cards_things_to_do = requests.request(
-#   "GET",
-#   get_cards,
-#   headers=headers,
-#   params=query
-#)
-
-
-list_id = all_lists.json()[1]['id']
-card_to_move = "https://api.trello.com/1/lists/{}/cards/".format(list_id)
-
-print(card_to_move)
-move_card = requests.request(
-   "PUT",
-   card_to_move,
-   headers=headers,
-   params=query
-)
-
-print(generate_list_string)
-"""
 @app.route('/', methods = ['POST', 'GET', 'PUT', 'DELETE'])
 def index():
     if request.method == 'POST':
