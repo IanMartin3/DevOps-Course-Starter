e20ded9ffde93d982e63b3654aaf82181ada513e
Ian Martin <ian.martin3@capita.com>
2021-02-11 11:32:33 +0000

    Removed POST and PUT from main endpoint as not required

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST', 'PUT'])
+@app.route('/', methods=['GET'])
 def index():
     return render_template(
         'index.html', 
