2449e7416e38a4f056d65c57674110858c659f28
Ian Martin <ian.martin3@capita.com>
2021-02-16 08:44:43 +0000

    Added functions to add cards to each list and removed unused function to get all boards

--- Trello.py
+++ Trello.py
@@ -1,3 +1,4 @@
+from flask import Flask, render_template, request, redirect, url_for
 import requests
 import os
 import json
@@ -17,17 +18,6 @@ trello_params = {
 """
 Get Trello Board 
 """
-def get_board():
-   get_boards = f"https://api.trello.com/1/boards/{os.getenv('TRELLO_BOARD_ID')}"
-   all_boards = requests.request(
-      "GET",
-      get_boards,
-      headers=headers,
-      params=trello_params
-   )
-
-   return all_boards.json()
-
 get_boards = "https://api.trello.com/1/members/me/boards"
 
 all_boards = requests.request(
@@ -94,3 +84,22 @@ post_things_to_do = f"https://api.trello.com/1/lists/{os.getenv('THINGS_TO_DO_LI
 post_doing = f"https://api.trello.com/1/lists/{os.getenv('DOING_LIST_ID')}/cards/"
 
 post_done = f"https://api.trello.com/1/lists/{os.getenv('DONE_LIST_ID')}/cards/"
+
+
+"""
+Add Cards to Lists on Trello Board 
+"""
+def add_thing_to_do(): 
+    NewThingToDo = request.form['NewThingToDo']
+    requests.post(post_things_to_do, params={**trello_params, 'name': NewThingToDo})
+    return redirect(url_for('index'))
+
+def add_doing(): 
+    NewDoingTask = request.form['NewDoingTask']
+    requests.post(post_doing, params={**trello_params, 'name': NewDoingTask})
+    return redirect(url_for('index'))
+
+def add_done(): 
+    NewDoneTask = request.form['NewDoneTask']
+    requests.post(post_done, params={**trello_params, 'name': NewDoneTask})
+    return redirect(url_for('index'))
