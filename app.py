c128445051d170642cce2742bd766bedbdfbaf7b
Ian Martin <ian.martin3@capita.com>
2021-04-12 10:13:15 +0100

    updated index to diplay using view_model

--- app.py
+++ app.py
@@ -2,50 +2,49 @@ from flask import Flask, render_template, request, redirect, url_for
 import requests
 import json
 import trello
-import os
-import moment
-from datetime import datetime
+from view_model import ViewModel
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    trello_api = trello.TrelloAPI()
+    card_call = trello.CardService()
+    @app.route('/', methods=['GET'])
+    def index():
+        items = card_call.card_details(trello_api.get_all_cards_on_this_board())
+        item_view_model = ViewModel(items)
+        return render_template(
+            'index.html',
+            all_my_boards = trello.boards(),
+            view_model=item_view_model
+        )
 
-@app.route('/', methods=['GET'])
-def index():
-    return render_template(
-        'index.html',
-        all_my_boards = trello.boards(),
-        all_my_lists = trello.lists_on_this_board(),
-        all_my_cards = trello.cards_on_this_board(),
-        todo_id = trello.todoid,
-        doing_id = trello.doingid,
-        done_id = trello.doneid
-    )
+    @app.route('/add_thing_to_do/', methods=['POST'])
+    def add_thing_to_do():
+        return trello.TrelloAPI.add_thing_to_do()
 
-@app.route('/add_thing_to_do/', methods=['POST'])
-def add_thing_to_do():
-    return trello.add_thing_to_do()
+    @app.route('/add_doing/', methods=['POST'])
+    def add_doing():
+        return trello.TrelloAPI.add_doing()
 
-@app.route('/add_doing/', methods=['POST'])
-def add_doing():
-    return trello.add_doing()
+    @app.route('/add_done/', methods=['POST'])
+    def add_done():
+        return trello.TrelloAPI.add_done() 
 
-@app.route('/add_done/', methods=['POST'])
-def add_done():
-    return trello.add_done() 
+    @app.route('/move_card_to_to_do/<id>')
+    def move_card_to_to_do(id):
+        trello.TrelloAPI.move_card_to_todo(id)
+        return redirect(url_for('index'))
 
-@app.route('/move_card_to_to_do/<id>')
-def move_card_to_to_do(id):
-    trello.move_card_to_todo(id)
-    return redirect(url_for('index'))
+    @app.route('/move_card_to_doing/<id>')
+    def move_card_to_doing(id):
+        trello.TrelloAPI.start_card(id)
+        return redirect(url_for('index'))
 
-@app.route('/move_card_to_doing/<id>')
-def move_card_to_doing(id):
-    trello.start_card(id)
-    return redirect(url_for('index'))
+    @app.route('/move_card_to_done/<id>')
+    def move_card_to_done(id):
+        trello.TrelloAPI.complete_card(id)
+        return redirect(url_for('index'))
+    
+    return app
 
-@app.route('/move_card_to_done/<id>')
-def move_card_to_done(id):
-    trello.complete_card(id)
-    return redirect(url_for('index'))
-
-if __name__ == '__main__':
-    app.run(debug=True)
+app = create_app()
